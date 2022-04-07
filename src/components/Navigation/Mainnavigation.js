429a3ad1b0366a32d88a7e96aa43e854360ace97
Vaibhav-VM-Vidhya <67579239+Vaibhav-VM-Vidhya@users.noreply.github.com>
2022-04-07 01:36:37 -0700

    navbar changes

--- src/components/Navigation/Mainnavigation.js
+++ src/components/Navigation/Mainnavigation.js
@@ -12,7 +12,7 @@ const Mainnavigation = () => {
 
             </div>
             <div className='flex items-center space-x-2 justify-center '>
-                <div className="md:flex flex  justify-center left-0  items-center md:space-x-4  fixed md:static md:mr-8 md:w-auto w-screen bottom-0   md:bg-none h-auto bg-blue-500 md:bg-gray-900 rounded-t-2xl">
+                <div className="md:flex flex  justify-center left-0  items-center md:space-x-4  fixed md:static md:mr-8 md:w-auto w-screen bottom-2   md:bg-none h-auto bg-blue-500 md:bg-gray-900 rounded-full ">
                     <div className="w-1/5 flex flex-col  justify-center items-center py-2 md:py-0   md:border-none md:flex-none">
                         <div className="md:hidden"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
